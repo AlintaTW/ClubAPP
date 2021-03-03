25291ec92575c9817c080ec0ce1cb6ec50ecc54a
Tran Quoc Viet <viettq@bachasoftware.com>
2021-03-03 21:41:38 +0700

    fix(app): localization - use i18n

--- src/localization/index.ts
+++ src/localization/index.ts
@@ -2,6 +2,7 @@ import i18n from 'i18n-js';
 import locales from './locales';
 
 i18n.defaultLocale = 'en';
+i18n.locale = 'en';
 i18n.fallbacks = true;
 i18n.translations = locales;
 i18n.missingBehaviour = 'guess';
