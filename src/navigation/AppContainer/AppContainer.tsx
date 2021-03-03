84971003080ec3fd5d552b4ee7fc24f9ffa5ccf2
Tran Quoc Viet <viettq@bachasoftware.com>
2021-03-03 17:04:26 +0700

    feat(screens): tab bar bottom - create tab bar bottom

--- src/navigation/AppContainer/AppContainer.tsx
+++ src/navigation/AppContainer/AppContainer.tsx
@@ -1,28 +1,21 @@
-import React from 'react'
-import {
-    NavigationContainer,
-} from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
 import MainTab from '../Stacks/MainTab';
 
 const Stack = createStackNavigator();
 
-const AppContainer = () => {
-    return (
-        <NavigationContainer>
-            <RootStack />
-        </NavigationContainer>
-    )
-}
+const AppContainer = () => (
+  <NavigationContainer>
+    <RootStack />
+  </NavigationContainer>
+);
 
-function RootStack() {
-    return (
-        <Stack.Navigator headerMode="none">
-            <>
-                <Stack.Screen component={MainTab} name="MainTab" />
-            </>
-        </Stack.Navigator>
-    )
-}
+const RootStack = () => (
+  <Stack.Navigator headerMode="none">
+    <Stack.Screen component={MainTab} name="MainTab" />
+  </Stack.Navigator>
+);
 
 export default AppContainer;
--- src/navigation/Stacks/MainTab.tsx
+++ src/navigation/Stacks/MainTab.tsx
@@ -1,10 +1,149 @@
-import React from 'react'
-import { View } from 'react-native'
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {StyleSheet, View} from 'react-native';
 
- const  MainTab = () => {
-    return (
-        <View />
-    )
-}
+import {Colors} from '@src/assets';
+import {HomeScreen} from '@src/screens';
+
+const Tab = createBottomTabNavigator();
+
+const MainTab = () => (
+  <Tab.Navigator
+    tabBarOptions={{
+      activeTintColor: Colors.readAlizarin,
+      inactiveTintColor: Colors.greyDrank,
+      labelStyle: {
+        paddingBottom: 5,
+        fontSize: 13,
+      },
+    }}>
+    <Tab.Screen
+      name="HOME"
+      options={{
+        title: 'Home',
+        unmountOnBlur: true,
+        tabBarIcon: ({color}) =>
+          color === Colors.readAlizarin ? (
+            <View style={styles.containerActive}>
+              <MaterialIcons
+                color={color}
+                name="home"
+                style={styles.iconActive}
+              />
+            </View>
+          ) : (
+            <MaterialIcons color={color} name="home" style={styles.icon} />
+          ),
+      }}
+      component={HomeScreen}
+    />
+    <Tab.Screen
+      name="HISTORY"
+      options={{
+        title: 'History',
+        unmountOnBlur: true,
+        tabBarIcon: ({color}) =>
+          color === Colors.readAlizarin ? (
+            <View style={styles.containerActive}>
+              <Ionicons
+                color={color}
+                name="ios-calendar"
+                style={styles.iconActive}
+              />
+            </View>
+          ) : (
+            <Ionicons color={color} name="ios-calendar" style={styles.icon} />
+          ),
+      }}
+      component={HomeScreen}
+    />
+    <Tab.Screen
+      name="FAVORITE"
+      options={{
+        title: 'Favorite',
+        unmountOnBlur: true,
+        tabBarIcon: ({color}) =>
+          color === Colors.readAlizarin ? (
+            <View style={styles.containerActive}>
+              <MaterialIcons
+                color={color}
+                name="favorite"
+                style={styles.iconActive}
+              />
+            </View>
+          ) : (
+            <MaterialIcons color={color} name="favorite" style={styles.icon} />
+          ),
+      }}
+      component={HomeScreen}
+    />
+    <Tab.Screen
+      name="NOTIFICATIONS"
+      options={{
+        title: 'Notification',
+        unmountOnBlur: true,
+        tabBarIcon: ({color}) =>
+          color === Colors.readAlizarin ? (
+            <View style={styles.containerActive}>
+              <MaterialIcons
+                color={color}
+                name="notifications"
+                style={styles.iconActive}
+              />
+            </View>
+          ) : (
+            <MaterialIcons
+              color={color}
+              name="notifications"
+              style={styles.icon}
+            />
+          ),
+      }}
+      component={HomeScreen}
+    />
+    <Tab.Screen
+      name="PROFILE"
+      options={{
+        title: 'Profile',
+        unmountOnBlur: true,
+        tabBarIcon: ({color}) =>
+          color === Colors.readAlizarin ? (
+            <View style={styles.containerActive}>
+              <MaterialIcons
+                color={color}
+                name="person"
+                style={styles.iconActive}
+              />
+            </View>
+          ) : (
+            <MaterialIcons color={color} name="person" style={styles.icon} />
+          ),
+      }}
+      component={HomeScreen}
+    />
+  </Tab.Navigator>
+);
+
+const styles = StyleSheet.create({
+  containerActive: {
+    alignItems: 'center',
+    backgroundColor: Colors.white,
+    borderRadius: 40,
+    display: 'flex',
+    height: 80,
+    justifyContent: 'center',
+    width: 80,
+  },
+  icon: {
+    fontSize: 25,
+    paddingTop: 5,
+  },
+  iconActive: {
+    fontSize: 30,
+    marginTop: -5,
+  },
+});
 
 export default MainTab;
