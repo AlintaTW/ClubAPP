6ad2d248fcc489589df9a852ebf62731a1ac77ed
Tran Quoc Viet <viettq@bachasoftware.com>
2021-03-04 15:40:16 +0700

    feat(app): dummy data - add dummy data

---
+++ src/untils/dummyData.ts
@@ -0,0 +1,24 @@
+import {Images} from '@src/assets';
+
+export const headerDats = [
+  {
+    id: '1',
+    title: '近距離 美語互動',
+    banner: Images.dummy.header,
+  },
+  {
+    id: '2',
+    title: '近距離 美語互動',
+    banner: Images.dummy.header,
+  },
+  {
+    id: '3',
+    title: '近距離 美語互動',
+    banner: Images.dummy.header,
+  },
+  {
+    id: '4',
+    title: '近距離 美語互動',
+    banner: Images.dummy.header,
+  },
+];
